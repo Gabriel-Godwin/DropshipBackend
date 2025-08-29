38d6d42a044c412a204ca3e69b60d82b818c1cca
Godwin Gabriel <goddygab.10@gmail.com>
2025-08-29 20:00:19 +0100

    Initial Spring Boot setup

---
+++ src/main/java/com/example/DropshipBackend/DropshipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DropshipBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropshipBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DropshipBackend/DropshipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DropshipBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropshipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
